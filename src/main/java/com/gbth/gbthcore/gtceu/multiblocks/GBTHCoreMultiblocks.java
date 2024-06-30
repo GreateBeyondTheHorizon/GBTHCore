61491abdb1c6aa9be4e5295155f198c7953c4424
Electrolyte220 <cjm03c@gmail.com>
2024-06-30 11:35:58 -0400

    Fix bloomery not forming

--- src/main/java/com/gbth/gbthcore/gtceu/multiblocks/GBTHCoreMultiblocks.java
+++ src/main/java/com/gbth/gbthcore/gtceu/multiblocks/GBTHCoreMultiblocks.java
@@ -7,11 +7,15 @@ import com.gregtechceu.gtceu.api.data.RotationState;
 import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
 import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
+import com.lowdragmc.lowdraglib.utils.BlockInfo;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CampfireBlock;
 
 import static com.gregtechceu.gtceu.common.data.GTBlocks.CASING_PRIMITIVE_BRICKS;
+import static net.minecraft.world.level.block.CampfireBlock.FACING;
+import static net.minecraft.world.level.block.CampfireBlock.LIT;
 
 public class GBTHCoreMultiblocks {
 
@@ -26,7 +30,8 @@ public class GBTHCoreMultiblocks {
                     .where("M", Predicates.blocks(Blocks.MUD_BRICKS))
                     .where("#", Predicates.air())
                     .where("C", Predicates.controller(Predicates.blocks(def.get())))
-                    .where("A", Predicates.states(Blocks.CAMPFIRE.defaultBlockState().setValue(CampfireBlock.LIT, false)))
+                    .where("A", Predicates.custom(s -> s.getBlockState().getBlock() instanceof CampfireBlock,
+                            () -> new BlockInfo[]{new BlockInfo(Blocks.CAMPFIRE.defaultBlockState().setValue(LIT, false)), new BlockInfo(Blocks.CAMPFIRE.defaultBlockState().setValue(LIT, false).setValue(FACING, Direction.NORTH))}))
                     .build())
             .workableCasingRenderer(new ResourceLocation("block/mud_bricks"), GTCEu.id("block/multiblock/primitive_blast_furnace"), false)
             .register();
