a2a10afe8fe7aead4ae6e657c6703f0e6be56248
lonevox <quinn.epicness@gmail.com>
2024-07-06 02:56:58 +1200

    Disable opening furnaces, blast furnaces, and smokers

--- src/main/java/com/gbth/gbthcore/mixin/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/com/gbth/gbthcore/mixin/AbstractFurnaceBlockEntityMixin.java
@@ -1,16 +1,20 @@
 package com.gbth.gbthcore.mixin;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.WorldlyContainer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.RecipeHolder;
 import net.minecraft.world.inventory.StackedContentsCompatible;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -23,11 +27,26 @@ public abstract class AbstractFurnaceBlockEntityMixin extends BaseContainerBlock
     }
 
     @Inject(method = "canPlaceItem", at = @At("HEAD"), cancellable = true)
-    private void gbthcore$canPlaceItem(int pIndex, ItemStack pStack, CallbackInfoReturnable<Boolean> cir) {
-        if(this.getBlockState().getBlock() == Blocks.FURNACE ||
-            this.getBlockState().getBlock() == Blocks.BLAST_FURNACE ||
-            this.getBlockState().getBlock() == Blocks.SMOKER) {
+    private void gbthcore$canPlaceItem(CallbackInfoReturnable<Boolean> cir) {
+        if (gbthcore$isDisabled()) {
             cir.setReturnValue(false);
         }
+    }
+
+    @Override
+    public boolean canOpen(Player pPlayer) {
+        if (gbthcore$isDisabled()) {
+            Component warningText = Component.literal("This block has no functionality!")
+                    .withStyle(ChatFormatting.RED);
+            pPlayer.displayClientMessage(warningText, true);
+            return false;
+        }
+        return super.canOpen(pPlayer);
+    }
+
+    @Unique
+    private boolean gbthcore$isDisabled() {
+        Block block = this.getBlockState().getBlock();
+        return block == Blocks.FURNACE || block == Blocks.BLAST_FURNACE || block == Blocks.SMOKER;
     }
 }
