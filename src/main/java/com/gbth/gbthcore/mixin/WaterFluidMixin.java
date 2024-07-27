1eede1576c63771728a185fae2ecde18b1a87264
lonevox <quinn.epicness@gmail.com>
2024-07-27 20:58:33 +1200

    Fix restrictWaterSpread option

--- src/main/java/com/gbth/gbthcore/mixin/WaterFluidMixin.java
+++ src/main/java/com/gbth/gbthcore/mixin/WaterFluidMixin.java
@@ -2,11 +2,8 @@ package com.gbth.gbthcore.mixin;
 
 import com.gbth.gbthcore.GBTHConfig;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.material.FlowingFluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.WaterFluid;
@@ -16,11 +13,14 @@ import org.spongepowered.asm.mixin.Mixin;
 public abstract class WaterFluidMixin extends FlowingFluid {
 	@Override
 	public boolean canConvertToSource(FluidState state, Level level, BlockPos pos) {
-		for (String biome : GBTHConfig.INSTANCE.allowedWaterSpreadBiomes) {
-			if (level.getBiome(pos).is(new ResourceLocation(biome))) {
-				return true;
+		if (GBTHConfig.INSTANCE.restrictWaterSpread) {
+			for (String biome : GBTHConfig.INSTANCE.allowedWaterSpreadBiomes) {
+				if (level.getBiome(pos).is(new ResourceLocation(biome))) {
+					return true;
+				}
 			}
+			return false;
 		}
-		return false;
+		return true;
 	}
 }
